921c08755ebd6edd52740f21d6e0215537fbccf9
lombardl <llomb72@gmail.com>
2021-03-04 12:25:41 +0100

    Final version

--- Testing.R
+++ Testing.R
@@ -1,5 +1,1 @@
-print("This file was created within RStudio")
-
-print("And now it lives on GitHub")
-
-print("And this is another edit to test authentication.")
+print("Now this file lives on GitHub")
